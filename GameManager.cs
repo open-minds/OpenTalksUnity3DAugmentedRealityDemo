13aefef12a1525bdf17cff6fd96130173963a7c4
Oussama Bonnor <oussamabonnor@gmail.com>
2018-04-13 17:13:38 +0100

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private Text text;
+    [SerializeField] private GameObject PokeBall;
+    [SerializeField] private GameObject Pekatchu;
+    [SerializeField] private float strenght = 10f;
+    [SerializeField] private bool IsBallShot = false;
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetMouseButtonDown(0) && !IsBallShot)
+	    {
+	        PokeBall.transform.parent = null;
+	        Rigidbody rb = PokeBall.GetComponent<Rigidbody>();
+	        rb.isKinematic = false;
+	        Vector3 force = gameObject.transform.position - PokeBall.transform.position;
+	        rb.velocity = force * strenght;
+            rb.rotation = new Quaternion(0,15f,5f,0);
+	        IsBallShot = true;
+	    }
+	}
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag.Equals("pokeball"))
+        {
+            StartCoroutine(Capture());
+        }
+    }
+
+    IEnumerator Capture()
+    {
+        for (int i = 10 - 1; i >= 0; i--)
+        {  
+            PokeBall.transform.localScale = new Vector3((float)i / 10, (float)i / 10, (float)i / 10);
+            gameObject.transform.localScale = new Vector3((float)i / 10, (float)i / 10, (float)i / 10);
+            yield return new WaitForSeconds(.1f);
+        }
+        text.text = "Let's see if you caught the pokemon...";
+        yield return new WaitForSeconds(2f);
+        for (int i = 0; i <= 10; i++)
+        {
+            Pekatchu.transform.localScale = new Vector3((float)i / 10, (float)i / 10, (float)i / 10);
+            yield return new WaitForSeconds(.05f);
+        }
+        yield return new WaitForSeconds(2f);
+        Pekatchu.GetComponent<Animator>().enabled = false;
+        text.text = "You caught the pokemon!";
+    }
+}
---
+++ SoldierSceneManager.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SoldierSceneManager : MonoBehaviour
+{
+    [SerializeField] private Text text;
+    [SerializeField] private Text endingText;
+    [SerializeField] private Image image;
+    [SerializeField] private GameObject Soldier;
+    [SerializeField] private GameObject Zombie;
+    [SerializeField] private GameObject Canvas;
+    [SerializeField] private float speed = 8f;
+    [SerializeField] private GameObject[] Ballons;
+    [SerializeField] private Transform[] BallonSpawner;
+    [SerializeField] private AudioSource musicPlayer;
+    private bool IsAttack;
+    private bool IsDance;
+    private bool IsTurn;
+    
+	// Update is called once per frame
+	void Update () {
+        //When we tap the phone
+	    if (Input.GetMouseButtonDown(0))
+	    {
+	        text.text = "Fight!";
+	        Soldier.GetComponent<Animator>().SetBool("Aim",true);
+	        Zombie.GetComponent<Animator>().SetBool("attack",true);
+	        IsAttack = true;
+	        StartCoroutine(endingAnimation());
+	    }
+
+	    #region WeirdCode
+	    if(IsAttack) Zombie.transform.position = Vector3.Lerp(Zombie.transform.position, Soldier.transform.position , Time.deltaTime / 4);
+	    if (IsDance)
+	    {
+	        Vector3 target = new Vector3(Camera.main.transform.position.x,
+	            1, Camera.main.transform.position.z);
+            Quaternion q = Quaternion.LookRotation(target - Zombie.transform.position);
+	        Zombie.transform.rotation = Quaternion.Lerp(Zombie.transform.rotation, q, Time.deltaTime);
+	        }
+	    if (IsTurn)
+	    {
+	        Vector3 target = new Vector3(Camera.main.transform.position.x,
+	            1, Camera.main.transform.position.z);
+	        Quaternion q = Quaternion.LookRotation(target - Soldier.transform.position);
+	        Soldier.transform.rotation = Quaternion.Lerp(Soldier.transform.rotation, q, Time.deltaTime);
+        }
+        //if(Canvas.activeInHierarchy) Canvas.transform.LookAt(Camera.main.transform);
+	    #endregion
+    }
+    
+    IEnumerator endingAnimation()
+    {
+        yield return new WaitForSeconds(2.5f); //Zombie walking
+        IsDance = true; 
+        IsAttack = false;
+        Zombie.GetComponent<Animator>().SetBool("attack", false); //Zombie stop walking
+        yield return new WaitForSeconds(2f); //Zombie turning
+        Zombie.GetComponent<Animator>().SetBool("dance",true); //Zombie dancing
+        StartCoroutine(spawningBallons());
+        yield return new WaitForSeconds(2f); 
+        IsTurn = true;  //SpaceBoy turning
+        yield return new WaitForSeconds(2f); //Awkward pause
+        Canvas.SetActive(true); //Talk popup
+    }
+
+    #region SecondWeirdCode
+    IEnumerator spawningBallons()
+    {
+        musicPlayer.Play();
+        for (int i = 0; i < 20; i++)
+        {
+            int index = Random.Range(0, BallonSpawner.Length);
+            GameObject ballon = Instantiate(Ballons[Random.Range(0, Ballons.Length)], BallonSpawner[index]);
+            ballon.GetComponent<Rigidbody>().velocity = ballon.transform.up * 10f;
+            Destroy(ballon, 3f);
+            yield return new WaitForSeconds((float) Random.Range(8,15)/10);
+        }
+    }
+    #endregion
+
+    public void loadSecretPorject()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
